092085f34d365628ed5b1c76fd1045921647014d
lawless-m <maht0x0r@xsmail.com>
2016-09-22 11:07:23 -0400

    fix deprecated warnings

--- Diet_Problem.jl
+++ Diet_Problem.jl
@@ -4,20 +4,20 @@ m = Model()
 #Food available
 S = ["brownies","ice cream","cola","cheese cake"]
 #Non-negativity
-@defVar(m, x[S] >= 0)
+@variable(m, x[S] >= 0)
 #Minimum calories
-@addConstraint(m, 400x["brownies"] + 200x["ice cream"] + 150x["cola"] + 500x["cheese cake"] >= 500)
+@constraint(m, 400x["brownies"] + 200x["ice cream"] + 150x["cola"] + 500x["cheese cake"] >= 500)
 #At least 6 grams of chocolate
-@addConstraint(m, 3x["brownies"] + 2x["ice cream"] >= 6)
+@constraint(m, 3x["brownies"] + 2x["ice cream"] >= 6)
 #At least 10 grams of sugar
-@addConstraint(m, 2x["brownies"] + 2x["ice cream"] + 4x["cola"] + 4x["cheese cake"] >= 10)
+@constraint(m, 2x["brownies"] + 2x["ice cream"] + 4x["cola"] + 4x["cheese cake"] >= 10)
 #At least 8 grams of fat
-@addConstraint(m, 2x["brownies"] + 4x["ice cream"] + 1x["cola"] + 5x["cheese cake"] >= 8)
+@constraint(m, 2x["brownies"] + 4x["ice cream"] + 1x["cola"] + 5x["cheese cake"] >= 8)
 #Minimize cost of consumption
-@setObjective(m, Min, 0.5x["brownies"] + 0.2x["ice cream"] + 0.3x["cola"] + 0.8x["cheese cake"])
+@objective(m, Min, 0.5x["brownies"] + 0.2x["ice cream"] + 0.3x["cola"] + 0.8x["cheese cake"])
 #Solve the optimization problem
 solve(m)
 #Determine consumption amounts
-println("variable values: ", getValue(x))
+println("variable values: ", getvalue(x))
 #Determine optimal cost of consumption
-println("Objetive value: ", getObjectiveValue(m))
+println("Objetive value: ", getobjectivevalue(m))
